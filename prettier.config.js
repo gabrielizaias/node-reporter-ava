6c04abbc6aef18fb6cfe98a268ff9e47a64a6a61
Gabriel Silva <git@gabe.id>
2023-12-12 09:23:51 +0100

    chore: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Options} */
+export default {
+	arrowParens: "always",
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: "auto",
+	endOfLine: "lf",
+	htmlWhitespaceSensitivity: "css",
+	jsxSingleQuote: false,
+	printWidth: 100,
+	proseWrap: "always",
+	quoteProps: "consistent",
+	semi: true,
+	singleAttributePerLine: false,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "all",
+	useTabs: true,
+};
